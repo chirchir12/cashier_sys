7f39e9b725ca67433c715c5fca86282afe6b0cd7
chirchir <chirchir7370@gmail.com>
2024-07-24 08:16:16 +0300

    logic to calculate total of products in busket

--- lib/cashier_sys/checkout.ex
+++ lib/cashier_sys/checkout.ex
@@ -11,4 +11,60 @@ defmodule CashierSys.Checkout do
     product = Product.get_product(code)
     %__MODULE__{busket | items: [product | items]}
   end
+
+  def total(%__MODULE__{items: items}) do
+    items
+    |> Enum.group_by(& &1.code)
+    |> Enum.map(&apply_discounts/1)
+    |> Enum.sum()
+    |> Float.round(2)
+  end
+
+  defp apply_discounts({"CF1", products} ) do
+    price_drop(products, 3, 2/3)
+  end
+
+  defp apply_discounts({"SR1", products}) do
+    bulk_discount(products, 3, 4.50)
+  end
+
+  defp apply_discounts({"GR1", products}) do
+    buy_one_get_one_free(products)
+  end
+
+  defp apply_discounts({_code, products}) do
+    Enum.sum(Enum.map(products, &get_product_price/1))
+  end
+
+  defp bulk_discount(products, threshhold, discount_price) do
+    if length(products) >= threshhold do
+      length(products) * discount_price
+    else
+      Enum.sum(Enum.map(products, &get_product_price/1))
+    end
+  end
+
+  defp price_drop(products, threshold, discount_rate) do
+    if length(products) >= threshold do
+      Enum.sum(Enum.map(products, &get_product_price/1)) * discount_rate
+    else
+      Enum.sum(Enum.map(products, &get_product_price/1))
+    end
+  end
+
+  defp buy_one_get_one_free(products) do
+    Enum.chunk_every(products, 2)
+    |> Enum.map(fn
+      [product, _product] -> product.price
+      [product] -> product.price  end)
+    |> Enum.sum()
+  end
+
+  defp get_product_price(%Product{price: price}) do
+    price
+  end
+
+  defp get_product_price(_) do
+    0
+  end
 end
